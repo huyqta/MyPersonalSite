1bc11aba9ebc0a6c3efa03b95c267c83f06547e1
Quan Trong Anh Huy <huyqta@live.com>
2017-08-11 17:53:47 +0700

    Code category function

--- AdminForm/Form1.Designer.cs
+++ AdminForm/Form1.Designer.cs
@@ -55,14 +55,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tabCategory = new System.Windows.Forms.TabPage();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.label10 = new System.Windows.Forms.Label();
-            this.txtCategoryName = new System.Windows.Forms.TextBox();
-            this.label11 = new System.Windows.Forms.Label();
-            this.treeCategories = new System.Windows.Forms.TreeView();
-            this.label12 = new System.Windows.Forms.Label();
-            this.txtCategoryDesc = new System.Windows.Forms.TextBox();
             this.btnAddCategory = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.treeCategories = new System.Windows.Forms.TreeView();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtCategoryDesc = new System.Windows.Forms.TextBox();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtParentCategory = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabBooks.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -77,7 +79,7 @@
             this.tabControl1.Controls.Add(this.tabCategory);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(736, 543);
@@ -89,9 +91,9 @@
             this.tabBooks.Controls.Add(this.groupBox2);
             this.tabBooks.Controls.Add(this.groupBox1);
             this.tabBooks.Location = new System.Drawing.Point(4, 22);
-            this.tabBooks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabBooks.Margin = new System.Windows.Forms.Padding(2);
             this.tabBooks.Name = "tabBooks";
-            this.tabBooks.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabBooks.Padding = new System.Windows.Forms.Padding(2);
             this.tabBooks.Size = new System.Drawing.Size(728, 517);
             this.tabBooks.TabIndex = 0;
             this.tabBooks.Text = "Books";
@@ -102,9 +104,9 @@
             this.groupBox2.Controls.Add(this.gridEbooks);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(2, 207);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox2.Size = new System.Drawing.Size(724, 308);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
@@ -115,7 +117,7 @@
             this.gridEbooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridEbooks.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gridEbooks.Location = new System.Drawing.Point(2, 15);
-            this.gridEbooks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridEbooks.Margin = new System.Windows.Forms.Padding(2);
             this.gridEbooks.Name = "gridEbooks";
             this.gridEbooks.RowTemplate.Height = 33;
             this.gridEbooks.Size = new System.Drawing.Size(720, 291);
@@ -146,9 +148,9 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(2, 2);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(724, 205);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
@@ -158,7 +160,7 @@
             // 
             this.cboCategory.FormattingEnabled = true;
             this.cboCategory.Location = new System.Drawing.Point(465, 35);
-            this.cboCategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboCategory.Margin = new System.Windows.Forms.Padding(2);
             this.cboCategory.Name = "cboCategory";
             this.cboCategory.Size = new System.Drawing.Size(258, 21);
             this.cboCategory.TabIndex = 4;
@@ -166,7 +168,7 @@
             // btnDownload
             // 
             this.btnDownload.Location = new System.Drawing.Point(448, 175);
-            this.btnDownload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(2);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(89, 27);
             this.btnDownload.TabIndex = 3;
@@ -177,7 +179,7 @@
             // btnUpload
             // 
             this.btnUpload.Location = new System.Drawing.Point(540, 174);
-            this.btnUpload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(2);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(89, 27);
             this.btnUpload.TabIndex = 3;
@@ -188,7 +190,7 @@
             // btnSubmit
             // 
             this.btnSubmit.Location = new System.Drawing.Point(632, 174);
-            this.btnSubmit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(2);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(89, 27);
             this.btnSubmit.TabIndex = 3;
@@ -209,7 +211,7 @@
             // txtReadOnline
             // 
             this.txtReadOnline.Location = new System.Drawing.Point(465, 61);
-            this.txtReadOnline.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtReadOnline.Margin = new System.Windows.Forms.Padding(2);
             this.txtReadOnline.Name = "txtReadOnline";
             this.txtReadOnline.Size = new System.Drawing.Size(258, 20);
             this.txtReadOnline.TabIndex = 1;
@@ -217,7 +219,7 @@
             // txtLinkThumbnail
             // 
             this.txtLinkThumbnail.Location = new System.Drawing.Point(465, 81);
-            this.txtLinkThumbnail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLinkThumbnail.Margin = new System.Windows.Forms.Padding(2);
             this.txtLinkThumbnail.Name = "txtLinkThumbnail";
             this.txtLinkThumbnail.Size = new System.Drawing.Size(258, 20);
             this.txtLinkThumbnail.TabIndex = 1;
@@ -225,7 +227,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(92, 107);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(632, 66);
@@ -234,7 +236,7 @@
             // txtLinkImage
             // 
             this.txtLinkImage.Location = new System.Drawing.Point(92, 81);
-            this.txtLinkImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLinkImage.Margin = new System.Windows.Forms.Padding(2);
             this.txtLinkImage.Name = "txtLinkImage";
             this.txtLinkImage.Size = new System.Drawing.Size(258, 20);
             this.txtLinkImage.TabIndex = 1;
@@ -242,7 +244,7 @@
             // txtLinkDownload
             // 
             this.txtLinkDownload.Location = new System.Drawing.Point(92, 61);
-            this.txtLinkDownload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLinkDownload.Margin = new System.Windows.Forms.Padding(2);
             this.txtLinkDownload.Name = "txtLinkDownload";
             this.txtLinkDownload.Size = new System.Drawing.Size(258, 20);
             this.txtLinkDownload.TabIndex = 1;
@@ -250,7 +252,7 @@
             // txtPublisher
             // 
             this.txtPublisher.Location = new System.Drawing.Point(92, 35);
-            this.txtPublisher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPublisher.Margin = new System.Windows.Forms.Padding(2);
             this.txtPublisher.Name = "txtPublisher";
             this.txtPublisher.Size = new System.Drawing.Size(258, 20);
             this.txtPublisher.TabIndex = 1;
@@ -258,7 +260,7 @@
             // txtAuthor
             // 
             this.txtAuthor.Location = new System.Drawing.Point(465, 16);
-            this.txtAuthor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(2);
             this.txtAuthor.Name = "txtAuthor";
             this.txtAuthor.Size = new System.Drawing.Size(258, 20);
             this.txtAuthor.TabIndex = 1;
@@ -266,7 +268,7 @@
             // txtTitle
             // 
             this.txtTitle.Location = new System.Drawing.Point(92, 16);
-            this.txtTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2);
             this.txtTitle.Name = "txtTitle";
             this.txtTitle.Size = new System.Drawing.Size(258, 20);
             this.txtTitle.TabIndex = 1;
@@ -358,36 +360,46 @@
             this.tabCategory.Controls.Add(this.treeCategories);
             this.tabCategory.Controls.Add(this.label11);
             this.tabCategory.Controls.Add(this.txtCategoryDesc);
+            this.tabCategory.Controls.Add(this.txtParentCategory);
+            this.tabCategory.Controls.Add(this.label13);
             this.tabCategory.Controls.Add(this.txtCategoryName);
             this.tabCategory.Controls.Add(this.label10);
             this.tabCategory.Location = new System.Drawing.Point(4, 22);
-            this.tabCategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabCategory.Margin = new System.Windows.Forms.Padding(2);
             this.tabCategory.Name = "tabCategory";
-            this.tabCategory.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabCategory.Padding = new System.Windows.Forms.Padding(2);
             this.tabCategory.Size = new System.Drawing.Size(728, 517);
             this.tabCategory.TabIndex = 1;
             this.tabCategory.Text = "Categories";
             this.tabCategory.UseVisualStyleBackColor = true;
             // 
-            // openFileDialog1
+            // btnAddCategory
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.btnAddCategory.Location = new System.Drawing.Point(609, 169);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(111, 23);
+            this.btnAddCategory.TabIndex = 5;
+            this.btnAddCategory.Text = "Add category";
+            this.btnAddCategory.UseVisualStyleBackColor = true;
+            this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click);
             // 
-            // label10
+            // label12
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(341, 31);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(78, 13);
-            this.label10.TabIndex = 0;
-            this.label10.Text = "Category name";
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(341, 82);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Description";
             // 
-            // txtCategoryName
+            // treeCategories
             // 
-            this.txtCategoryName.Location = new System.Drawing.Point(425, 28);
-            this.txtCategoryName.Name = "txtCategoryName";
-            this.txtCategoryName.Size = new System.Drawing.Size(295, 20);
-            this.txtCategoryName.TabIndex = 1;
+            this.treeCategories.Location = new System.Drawing.Point(8, 28);
+            this.treeCategories.Name = "treeCategories";
+            this.treeCategories.Size = new System.Drawing.Size(327, 481);
+            this.treeCategories.TabIndex = 3;
+            this.treeCategories.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeCategories_AfterSelect);
+            this.treeCategories.Click += new System.EventHandler(this.treeCategories_Click);
             // 
             // label11
             // 
@@ -398,39 +410,49 @@
             this.label11.TabIndex = 2;
             this.label11.Text = "Category tree";
             // 
-            // treeCategories
-            // 
-            this.treeCategories.Location = new System.Drawing.Point(8, 28);
-            this.treeCategories.Name = "treeCategories";
-            this.treeCategories.Size = new System.Drawing.Size(327, 481);
-            this.treeCategories.TabIndex = 3;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(341, 57);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(60, 13);
-            this.label12.TabIndex = 4;
-            this.label12.Text = "Description";
-            // 
             // txtCategoryDesc
             // 
-            this.txtCategoryDesc.Location = new System.Drawing.Point(425, 54);
+            this.txtCategoryDesc.Location = new System.Drawing.Point(425, 79);
             this.txtCategoryDesc.Multiline = true;
             this.txtCategoryDesc.Name = "txtCategoryDesc";
             this.txtCategoryDesc.Size = new System.Drawing.Size(295, 84);
             this.txtCategoryDesc.TabIndex = 1;
             // 
-            // btnAddCategory
+            // txtCategoryName
             // 
-            this.btnAddCategory.Location = new System.Drawing.Point(609, 144);
-            this.btnAddCategory.Name = "btnAddCategory";
-            this.btnAddCategory.Size = new System.Drawing.Size(111, 23);
-            this.btnAddCategory.TabIndex = 5;
-            this.btnAddCategory.Text = "Add category";
-            this.btnAddCategory.UseVisualStyleBackColor = true;
-            this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click);
+            this.txtCategoryName.Location = new System.Drawing.Point(425, 53);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(295, 20);
+            this.txtCategoryName.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(341, 56);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Category name";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(341, 31);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(38, 13);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Parent";
+            // 
+            // txtParentCategory
+            // 
+            this.txtParentCategory.Location = new System.Drawing.Point(425, 28);
+            this.txtParentCategory.Name = "txtParentCategory";
+            this.txtParentCategory.Size = new System.Drawing.Size(295, 20);
+            this.txtParentCategory.TabIndex = 1;
             // 
             // Form1
             // 
@@ -438,7 +460,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 543);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Administrator tool";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -491,6 +513,8 @@
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.TextBox txtCategoryDesc;
         private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.TextBox txtParentCategory;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- AdminForm/Form1.cs
+++ AdminForm/Form1.cs
@@ -20,6 +20,7 @@ namespace AdminForm
         {
             InitializeComponent();
             LoadListBooks();
+            LoadCategories();
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
@@ -66,11 +67,37 @@ namespace AdminForm
         private void LoadCategories()
         {
             treeCategories.Nodes.Clear();
-            foreach (var cat in context.Categories.ToList())
+            var rootNode = Guid.Parse("00000000-0000-0000-0000-000000000000");
+            var listRoot = context.Categories.Where(c => c.ParentId == rootNode).ToList();
+            foreach(var cat in listRoot)
             {
-                treeCategories.Nodes.Add(new TreeNode() { Name = cat.Id.ToString(), Text = cat.Name });
+                TreeNode node = new TreeNode() { Text = cat.Name, Name=cat.Id.ToString() };
+                treeCategories.Nodes.Add(node);
+            }
+            foreach (TreeNode node in treeCategories.Nodes)
+            {
+                Guid nodeId = Guid.Parse(node.Name.ToString());
+                List<Category> categories = context.Categories.Where(c => c.ParentId == nodeId).ToList();
+                if (categories.Count > 0)
+                {
+                    AddChildNodes(categories, node);
+                }
             }
             
+        }
+
+        private void AddChildNodes(List<Category> nodes, TreeNode node)
+        {
+            foreach (Category cat in nodes)
+            {
+                TreeNode childNode = new TreeNode() { Text = cat.Name, Name = cat.Id.ToString() };
+                node.Nodes.Add(childNode);
+                var childNodes = context.Categories.Where(c => c.ParentId == cat.Id).ToList();
+                if (childNodes.Count > 0)
+                {
+                    AddChildNodes(childNodes, childNode);
+                }
+            }
         }
 
         private void btnUpload_Click(object sender, EventArgs e)
@@ -88,7 +115,8 @@ namespace AdminForm
 
         private void btnAddCategory_Click(object sender, EventArgs e)
         {
-            Guid parentId = treeCategories.SelectedNode != null ? Guid.Parse(treeCategories.SelectedNode.Name) : Guid.Parse("00000000-0000-0000-0000-000000000000");
+            var parentKey = context.Categories.Where(c => c.Name == txtParentCategory.Text).FirstOrDefault().Id;
+            Guid parentId = !string.IsNullOrEmpty(txtParentCategory.Text) ? Guid.Parse(treeCategories.Nodes.Find(parentKey.ToString(), true).FirstOrDefault().Name) : Guid.Parse("00000000-0000-0000-0000-000000000000");
             Category cat = new Category();
             cat.Id = Guid.NewGuid();
             cat.Name = txtCategoryName.Text;
@@ -97,6 +125,16 @@ namespace AdminForm
             context.Categories.Add(cat);
             context.SaveChanges();
             LoadCategories();
+        }
+
+        private void treeCategories_Click(object sender, EventArgs e)
+        {
+            //txtParentCategory.Text = treeCategories.SelectedNode.Text;
+        }
+
+        private void treeCategories_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            txtParentCategory.Text = treeCategories.SelectedNode.Text;
         }
     }
 }
